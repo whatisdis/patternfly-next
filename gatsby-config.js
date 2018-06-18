ed65602952e50de6a3c33b79ff3cccbe30209721
Joachim <jschuler@redhat.com>
2018-06-18 13:24:13 -0400

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,14 @@ const plugins = [
     options: {
       directory: `${__dirname}/src/patternfly`
     }
-  }
+  },
+  {
+    resolve: 'gatsby-plugin-netlify-cms',
+    options: {
+      modulePath: `${__dirname}/src/cms/cms.js`
+    }
+  },
+  'gatsby-plugin-netlify' // make sure to keep it last in the array
 ];
 
 module.exports = {
---
+++ src/cms/cms.js
@@ -0,0 +1,4 @@
+import CMS from 'netlify-cms';
+import PagePreview from './PagePreview';
+
+CMS.registerPreviewTemplate('page-preview', PagePreview);
